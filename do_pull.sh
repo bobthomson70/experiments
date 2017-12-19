81b5dcb6cb90265cd299f3e7bdd75fe2b3ef5d19
Bob Thomson <bobthomson70@gmail.com>
2017-12-19 20:57:39 +0000

    inventory vars

--- do_pull.sh
+++ do_pull.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/ansible-pull playbooks/deployment.yml -C master -d /tmp/deployment -U git@github.com:bobthomson70/experiments.git -i /tmp/deployment/inventory/machines/nodes.yml
+/usr/local/bin/ansible-pull playbooks/deployment.yml -C master -d /tmp/deployment -U git@github.com:bobthomson70/experiments.git -i /tmp/deployment/inventory/machines.yml
